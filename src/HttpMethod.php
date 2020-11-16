ded5aec8b95a43830f4675de49e806350cde1f4f
Lukasz Grajewski <dev3@melle.com>
2020-11-16 18:14:48 +0100

    Add HttpMethod class

---
+++ src/HttpMethod.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gooyer\Http;
+
+class HttpMethod
+{
+    public const GET = "GET";
+    public const POST = "POST";
+    public const PUT = "PUT";
+    public const DELETE = "DELETE";
+    public const PATCH = "PATCH";
+    public const HEAD = "HEAD";
+    public const OPTIONS = "OPTIONS";
+}
